51d616b1921a0b402538138a92649faf75052ff3
Coglia <16285696@itisrossi.vi.it>
2026-03-24 10:49:24 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "operazioni.h"
+
+int main() {
+    printf("Somma: %d\n",     somma(4, 3));
+    printf("Differenza: %d\n", differenza(4, 3));
+    return 0;
+}
+
---
+++ operazioni.c
@@ -0,0 +1,7 @@
+//
+// Created by 16285696 on 24/03/2026.
+//
+#include "operazioni.h"ù
+int somma(int a, int b) {
+    return a + b;
+}
